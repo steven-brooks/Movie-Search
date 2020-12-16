d9a687cf7458978bef6b1bad27bd8842ec30d814
Steven Brooks <steven@stevenbrooks.net>
2020-12-16 09:32:17 -0600

    Initial Commit

---
+++ Movie Search/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Movie Search
+//
+//  Created by Steven Brooks on 12/16/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
